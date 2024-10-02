a7f9b2746e38822d3e4972031197f8396a21bea3
Daniel Próspero <daniel.ribeiro@elev.tec.br>
2024-10-02 19:32:28 -0300

    Imprementando a tela de listar

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -4,5 +4,41 @@
     <h2>Listar os cursos</h2>
  
     <a href="{{ route('courses.create') }}">Criar um curso</a> <br>
-    <a href="{{ route('courses.show') }}">Visualizar</a> <br>
+
+    @if (session('success'))
+        <p style="color: green">{{ session('success') }}</p>
+    @endif
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Criado em</th>
+                <th>Atualizado em</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($courses as $course)
+                <tr>
+                    <td>{{ $course->id }}</td>
+                    <td>{{ $course->name }}</td>
+                    <td>{{ $course->created_at->format('d/m/Y H:i:s') }}</td>
+                    <td>{{ $course->updated_at->format('d/m/Y H:i:s') }}</td>
+                    <td>
+                        <a href="{{ route('courses.show', ['course' => $course->id]) }}">Visualizar</a>
+                        <a href="{{ route('courses.edit', ['course' => $course->id]) }}">Editar</a>
+                        <a href="{{ route('courses.destroy', ['course' => $course->id]) }}" onclick="event.preventDefault(); if (confirm('Deseja excluir o curso?')) { document.getElementById('form-course-destroy-{{ $course->id }}').submit(); }">Excluir</a>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="5">Nenhum curso cadastrado</td>
+                </tr>
+            @endforelse
+
+            {{ $courses->links() }}
+        </tbody>
+
 @endsection
--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.admin') {{-- Certifique-se de estar estendendo um layout, se necessário --}}
 
 @section('content')
-    <h2>Listar os cursos</h2>
+    <h2>Visualizar o curso</h2>
+
+
+    <p>ID: {{ $course->id }}</p>
+    <p>Nome: {{ $course->name }}</p>
+    <p>Criado em: {{ $course->created_at->format('d/m/Y H:i:s') }}</p>
+    <p>Atualizado em: {{ $course->updated_at->format('d/m/Y H:i:s') }}</p>
+
  
     <a href="{{ route('courses.index') }}">Listar</a> <br>
     <a href="{{ route('courses.edit') }}">Editar</a> <br>
